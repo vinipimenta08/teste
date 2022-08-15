157fe17e6024b75147fb46ee3edb82b15c2b5b0e
Vinicius Pimenta <vinicius.pimenta@outlook.com.br>
2022-08-15 20:04:00 -0300

    adding screen and registering a new user

---
+++ resources/lang/pt-br/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    "titleStoreError" => "Erro ao salvar registro",
+    "titleSearchError" => "Erro ao realizar a busca",
+    "titleUpdateError" => "Erro ao atualizar registro",
+    "titleDestroyError" => "Erro ao deletar registro",
+    "titleActiveError" => "Erro ao :status registro",
+    "titleLoadPageError" => "Erro ao carregar a página",
+    "titleResendError" => "Erro ao realizar o disparo",
+    "titleValidateFileError" => "Erro na validação do arquivo",
+    "titleEndProcessError" => "Erro ao finalizar o processo",
+    "titleDownloadFileError" => "Erro ao realizar o download do arquivo",
+    "defaultMessage" => "Favor entrar em contato caso o erro persista",
+];
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function(){
+
+    Route::get('/', [HomeController::class, 'index']);
+
+    Auth::routes();
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
 });
